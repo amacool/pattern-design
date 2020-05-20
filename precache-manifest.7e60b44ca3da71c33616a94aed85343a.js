a2b81de659766d9536c86e3a07fab05022b228c4
Amacool <amacool0117@outlook.com>
2020-05-20 22:39:13 +0200

    Updates

---
+++ precache-manifest.7e60b44ca3da71c33616a94aed85343a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d618e8f8704a979781bf343a5dc3074",
+    "url": "/pattern-design/index.html"
+  },
+  {
+    "revision": "6a0d8b9112540404216f",
+    "url": "/pattern-design/static/css/main.8f4c153c.chunk.css"
+  },
+  {
+    "revision": "20661ca72d65f8cef771",
+    "url": "/pattern-design/static/js/2.13c9d7fe.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/pattern-design/static/js/2.13c9d7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0d8b9112540404216f",
+    "url": "/pattern-design/static/js/main.dbccd825.chunk.js"
+  },
+  {
+    "revision": "487c815ba410bf3e1efc",
+    "url": "/pattern-design/static/js/runtime-main.201cce3d.js"
+  }
+]);
